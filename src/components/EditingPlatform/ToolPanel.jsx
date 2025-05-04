b3b47239a71eefd93952188a9378e579871ba3b4
Priya Pradhan <priyapradhan437@gmsil.com>
2025-05-04 18:40:49 +0530

    adding after updating changes

--- src/components/EditingPlatform/ToolPanel.jsx
+++ src/components/EditingPlatform/ToolPanel.jsx
@@ -311,6 +311,7 @@ export default function ToolPanel({
   );
 
  
+  
 const renderAdjustToolPanel = () => (
   <div className="rounded-lg overflow-hidden">
     <h2 className="text-gray-200 font-medium mb-2 text-sm">Size & Orientation</h2>

855dd28d4bb8c66448b42c1212f37fa6da24408a
Priya Pradhan <priyapradhan437@gmsil.com>
2025-07-30 17:38:01 +0530

    store created

--- src/app/editp/page.js
+++ src/app/editp/page.js
@@ -1,7 +1,9 @@
-"use client"
-import Editpt from '../../pages/EditingPlatform'
-
-
+// "use client"
+// import Editpt from '../../pages/EditingPlatform'
+import dynamic from 'next/dynamic';
+const Editpt  = dynamic(() => import('../../pages/EditingPlatform'), {
+  ssr: false
+});
 export default function editpt() {
   return (
     <>
